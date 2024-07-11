c6344bfc36799a86121c9c799ccf186cac683407
Alan Met <106497267+AlanMet@users.noreply.github.com>
2024-07-11 23:44:23 +0100

    Update networks.dart

--- networks.dart
+++ networks.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
 import 'matrices.dart';
 
 class Network {
   int? seed;
   late List<int> _architecture;
-  late List<double Function(double)> _activations;
+  late List<Matrix Function(Matrix)> _activations;
   List<Matrix> _weights = [],
       _biases = [],
       _preActivated = [],
@@ -12,24 +13,22 @@ class Network {
       _gradb = [],
       _deltas = [];
 
-  Network(List<int> architecture, List<double Function(double)> activations) {
+  Network(List<int> architecture, List<Matrix Function(Matrix)> activations) {
     _architecture = architecture;
     _activations = activations;
 
     for (int x = 0; x < _architecture.length - 1; x++) {
       _weights.add(randn(_architecture[x], _architecture[x + 1]));
-      print(_weights[x].toString());
-      _biases.add(randn(1, _architecture[x + 1]));
-      print(_biases[x].toString());
+      _biases.add(zeros(1, _architecture[x + 1]));
     }
   }
 
-  Matrix _activation(Matrix input, double Function(double) function) {
-    return input.performFunction(function);
+  Matrix _activation(Matrix input, Matrix Function(Matrix) function) {
+    return function(input);
   }
 
-  Matrix _activation_deriv(Matrix input, double Function(double) function) {
-    return input.performFunction(derivative(function));
+  Matrix _activationDeriv(Matrix input, Matrix Function(Matrix) function) {
+    return derivative(function)(input);
   }
 
   double mse(Matrix x, Matrix y) {
@@ -48,6 +47,7 @@ class Network {
       _activated.add(_activation(_preActivated[i + 1], _activations[i]));
     }
 
+    //print(_activated[_activated.length - 1].toString());
     return _activated[_activated.length - 1];
   }
 
@@ -60,20 +60,23 @@ class Network {
     _gradb = [];
     _deltas = [];
 
-    //not my algorithm
-    _deltas.add(_activated[_activated.length - 1] - y);
+    //dC/dz
+    _deltas.add(_activated.last - y);
 
-    _gradw.add(dot(_activated[_activated.length - 2].transpose(),
-        _deltas[_deltas.length - 1]));
+    //dC/dz*dz/dw
+    _gradw
+        .add(dot(_activated[_activated.length - 2].transpose(), _deltas.last));
 
-    _gradb.add(sum(_deltas[_deltas.length - 1], 0));
+    //sum dC/dz
+    _gradb.add(sum(_deltas.last, 0));
 
     for (var i = _architecture.length - 2; i > 0; i--) {
-      _deltas.add(dot(_deltas[_deltas.length - 1], _weights[i].transpose()) *
-          _activation_deriv(_preActivated[i], _activations[i]));
-      _gradw
-          .add(dot(_activated[i - 1].transpose(), _deltas[_deltas.length - 1]));
-      _gradb.add(sum(_deltas[_deltas.length - 1], 0));
+      //dc/dz * dz/da * da/dz
+      _deltas.add(dot(_deltas.last, _weights[i].transpose()) *
+          _activationDeriv(_preActivated[i], _activations[i]));
+      //dC/dz * dz/da * da/dz * dz/dw
+      _gradw.add(dot(_activated[i - 1].transpose(), _deltas.last));
+      _gradb.add(sum(_deltas.last, 0));
     }
 
     _gradw = _gradw.reversed.toList();
@@ -90,13 +93,44 @@ class Network {
 
   void train(
       List<Matrix> inputs, List<Matrix> expected, double lr, int epochs) {
+    print("beginning training");
     for (var i = 0; i < epochs; i++) {
-      _forward(inputs[i]);
-      _backward(inputs[i], expected[i]);
-      update(lr);
-      if (i % 100 == 0) {
-        print("epoch ${i}: ${mse(inputs[i], expected[i])}");
+      for (var x = 0; x < inputs.length; x++) {
+        _forward(inputs[x]);
+        _backward(inputs[x], expected[x]);
+        update(lr);
       }
+      print("epoch $i: ${mse(inputs[0], expected[0])}");
     }
   }
+
+  void store(String name) {
+    String filePath = '$name.txt';
+    File file = File(filePath);
+
+    if (!file.existsSync()) {
+      file.createSync(recursive: true);
+    }
+
+    String content = "";
+    for (var weight in _weights) {
+      content += weight.toString();
+    }
+
+    file.writeAsStringSync(content);
+
+    print('Matrix stored successfully in $filePath');
+  }
+}
+
+void main() {
+  /*Network net = Network([2, 2, 2], [relu, softmax]);
+
+  Matrix input = randn(1, 2);
+  Matrix output = randn(1, 2);
+
+  print(input.toString());
+  print(net._forward(input).toString());
+  net._backward(input, output);
+  print(net._forward(input).toString());*/
 }
