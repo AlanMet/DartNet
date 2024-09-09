0558aa3fc79a354a8386142263e6768231605d8c
AlanMet <106497267+AlanMet@users.noreply.github.com>
2024-09-09 09:00:10 +0100

    docs

--- matrices.dart
+++ matrices.dart
@@ -29,6 +29,10 @@ class Matrix {
     }
   }
 
+  /// Create a new empty matrix
+  /// - [row] The number of rows in the matrix
+  /// - [col] The number of columns in the matrix
+  /// - Returns The matrix
   Matrix(int row, int col) {
     _row = row;
     _col = col;
@@ -58,22 +62,52 @@ class Matrix {
     return (_matrix[row][col]).toDouble();
   }
 
+  /// Set the value of a matrix at a specific index
+  /// - [row] The row index
+  /// - [col] The column index
+  /// - [value] The value to set
+  /// - Example
+  /// ```dart
+  /// Matrix matrix = Matrix(2, 2);
+  /// matrix.setAt(0, 0, value: 1);
+  /// ```
   void setAt(int row, int col, {required double value}) {
     _matrix[row][col] = value;
   }
 
+  /// Create an empty matrix
+  /// - Example
+  /// ```dart
+  /// Matrix matrix = Matrix(2, 2); //Empty is called by default
+  /// ```
   void empty() {
     _matrix = List<List<double>>.generate(_row,
         (i) => List<double>.generate(_col, (index) => 0.0, growable: false),
         growable: false);
   }
 
+  /// Fill the matrix with a specific number
+  /// - [num] The number to fill the matrix with
+  /// - Example
+  /// ```dart
+  /// Matrix matrix = Matrix(2, 2);
+  /// matrix.fill(1);
+  /// ```
   void fill(double num) {
     _matrix = List<List<double>>.generate(_row,
         (i) => List<double>.generate(_col, (index) => num, growable: false),
         growable: false);
   }
 
+  /// Fills the matrix with random doubles
+  /// - [min] The minimum value
+  /// - [max] The maximum value
+  /// - [seed] The optional seed
+  /// - Example
+  /// ```dart
+  /// Matrix matrix = Matrix(2, 2);
+  /// matrix.generateDouble(0, 1);
+  /// ```
   void generateDouble(double min, double max, {int? seed}) {
     Random rand = Random(seed);
     _matrix = List<List<double>>.generate(
@@ -84,6 +118,16 @@ class Matrix {
         growable: false);
   }
 
+  /// Perform a function on the matrix
+  /// - [function] The function to perform
+  /// - Returns The new matrix
+  /// - Example
+  /// ```dart
+  /// Matrix matrix = Matrix(2, 2);
+  /// Matrix newMatrix = matrix.performFunction((a) => a * 2);
+  /// ```
+  /// - Note
+  /// The function should take a double as input and return a double
   Matrix performFunction(Function(double) function) {
     Matrix newMatrix = Matrix(_row, _col);
     for (int i = 0; i < _row; i++) {
@@ -95,7 +139,18 @@ class Matrix {
     return newMatrix;
   }
 
-  //same as above but takes a matrix as input
+  /// Perform an operation on the matrix
+  /// - [matrixB] The matrix to perform the operation with
+  /// - [operation] The operation to perform
+  /// - Returns The new matrix
+  /// - Example
+  /// ```dart
+  /// Matrix matrixA = Matrix(2, 2);
+  /// Matrix matrixB = Matrix(2, 2);
+  /// Matrix newMatrix = matrixA._performOperation(matrixB, (a, b) => a + b);
+  /// ```
+  /// - Note
+  /// The operation should take two doubles as input and return a double
   Matrix _performOperation(
       Matrix matrixB, double Function(double, double) operation) {
     if (_row != matrixB.getRow() || _col != matrixB.getCol()) {
@@ -113,6 +168,8 @@ class Matrix {
     return newMatrix;
   }
 
+  /// Transpose the matrix
+  /// - Returns The new matrix
   Matrix transpose() {
     Matrix newMatrix = Matrix(_col, _row);
     for (int i = 0; i < _row; i++) {
@@ -123,6 +180,8 @@ class Matrix {
     return newMatrix;
   }
 
+  /// Flatten the matrix
+  /// - Returns The new matrix
   Matrix flatten() {
     Matrix newMatrix = Matrix(_row, 1);
     for (var row in _matrix) {
@@ -137,6 +196,9 @@ class Matrix {
     return newMatrix;
   }
 
+  /// Dot product of two matrices
+  /// - [matrixB] The matrix to perform the dot product with
+  /// - Returns The new matrix
   Matrix dot(Matrix matrixB) {
     if (getDimensions()[1] != matrixB.getDimensions()[0]) {
       throw Exception(
@@ -154,34 +216,55 @@ class Matrix {
     return newMatrix;
   }
 
+  /// Add two matrices
+  /// - [matrixB] The matrix to add
+  /// - Returns The new matrix
   Matrix add(Matrix matrixB) {
     return _performOperation(matrixB, (a, b) => a + b);
   }
 
+  /// Subtract two matrices
+  /// - [matrixB] The matrix to subtract
+  /// - Returns The new matrix
   Matrix subtract(Matrix matrixB) {
     return _performOperation(matrixB, (a, b) => a - b);
   }
 
+  /// Divide two matrices
+  /// - [matrixB] The matrix to divide
+  /// - Returns The new matrix
   Matrix divide(Matrix matrixB) {
     return _performOperation(matrixB, (a, b) => a / b);
   }
 
+  /// Divide the matrix by a scalar
+  /// - [x] The scalar to divide by
+  /// - Returns The new matrix
   Matrix scalarDivide(double x) {
     Matrix matrixB = Matrix(_row, _col);
     matrixB.fill(1 / x);
     return hadamardProduct(matrixB);
   }
 
+  /// Multiply the matrix by a scalar
+  /// - [x] The scalar to multiply by
+  /// - Returns The new matrix
   Matrix multiply(double x) {
     Matrix matrixB = Matrix(_row, _col);
     matrixB.fill(x);
     return hadamardProduct(matrixB);
   }
 
+  /// Multiply two matrices
+  /// - [matrixB] The matrix to multiply
+  /// - Returns The new matrix
   Matrix hadamardProduct(Matrix matrixB) {
     return _performOperation(matrixB, (a, b) => a * b);
   }
 
+  /// Sum the matrix
+  /// - [axis] The axis to sum
+  /// - Returns The new matrix
   Matrix sum({required int axis}) {
     Matrix matrix = Matrix(_row, 1);
     if (axis == 1) {
@@ -205,6 +288,9 @@ class Matrix {
     return matrix;
   }
 
+  /// Check if two matrices are equivalent
+  /// - [matrixB] The matrix to compare
+  /// - Returns True if the matrices are equivalent
   bool isEquivalent(Matrix matrxiB) {
     if (matrxiB._col != _col && matrxiB._row != _row) {
       return false;
@@ -232,35 +318,63 @@ class Matrix {
   join(String s) {}
 }
 
+/// Create a matrix with random values
+/// - [row] The number of rows in the matrix
+/// - [col] The number of columns in the matrix
+/// - [seed] The optional seed
+/// - Returns The matrix
 Matrix randn(int row, int col, {int? seed}) {
   Matrix matrix = Matrix(row, col);
   matrix.generateDouble(0, 1, seed: seed);
   return matrix;
 }
 
+/// Create a matrix with zeros
+/// - [row] The number of rows in the matrix
+/// - [col] The number of columns in the matrix
+/// - Returns The matrix
 Matrix zeros(int row, int col) {
   return Matrix(row, col);
 }
 
+/// Create a matrix with a num
+/// - [num] The number to fill the matrix with
+/// - [row] The number of rows in the matrix
+/// - [col] The number of columns in the matrix
 Matrix fill(num num, int row, int col) {
   Matrix matrix = Matrix(row, col);
   matrix.fill(num.toDouble());
   return matrix;
 }
 
+/// scalar power of a matrix
+/// - [matrix] The matrix to perform the operation on
+/// - [x] The power to raise the matrix to
+/// - Returns The new matrix
 Matrix power(Matrix matrix, int x) {
   return matrix.performFunction((a) => pow(a, x));
 }
 
+///dot product of two matrices
+/// - [matrixA] The first matrix
+/// - [matrixB] The second matrix
+/// - Returns The new matrix
 Matrix dot(Matrix matrixA, Matrix matrixB) {
   return matrixA.dot(matrixB);
 }
 
+///sum of a matrix
+/// - [matrix] The matrix to sum
+/// - [axis] The axis to sum
+/// - Returns The new matrix
 dynamic sum(Matrix matrix, int axis) {
   Matrix newMatrix = matrix.sum(axis: axis);
   return newMatrix;
 }
 
+///mean of a matrix
+/// - [matrix] The matrix to find the mean
+/// - Returns The mean
 double mean(Matrix matrix) {
   double total = matrix.sum(axis: 0).sum(axis: 1).getAt(0, 0);
   double average =
@@ -268,60 +382,102 @@ double mean(Matrix matrix) {
   return average;
 }
 
+///exponential of a matrix
+/// - [matrix] The matrix to find the exponential
+/// - Returns The new matrix
 Matrix exponential(Matrix matrix) {
   return matrix.performFunction((a) => exp(a));
 }
 
+///sigmoid of a matrix
+/// - [matrix] The matrix to find the sigmoid
+/// - Returns The new matrix
 Matrix sigmoid(Matrix matrix) {
   return matrix.performFunction((x) => 1 / (1 + exp(-x)));
 }
 
+///derivative of a sigmoid matrix
+/// - [matrix] The matrix to find the derivative
+/// - Returns The new matrix
 Matrix sigmoidDeriv(Matrix matrix) {
   return matrix.performFunction(
       (x) => ((1 / (1 + exp(-x))) * (1 - (1 / (1 + exp(-x))))));
 }
 
+///tanh of a matrix
+/// - [matrix] The matrix to find the tanh
+/// - Returns The new matrix
 Matrix softmax(Matrix matrix) {
   return exponential(matrix) / sum(exponential(matrix), 1).getAt(0, 0);
 }
 
+///derivative of a tanh matrix
+/// - [matrix] The matrix to find the derivative
+/// - Returns The new matrix
 Matrix softmaxDeriv(Matrix matrix) {
   Matrix newMatrix = fill(1, matrix.getRow(), matrix.getCol());
   return matrix * (newMatrix - matrix);
 }
 
+///tanh of a matrix
+/// - [matrix] The matrix to find the tanh
+/// - Returns The new matrix
 Matrix tanH(Matrix matrix) {
   return matrix.performFunction((x) => (exp(2 * x) - 1) / (exp(2 * x) + 1));
 }
 
+///derivative of a tanh matrix
+/// - [matrix] The matrix to find the derivative
+/// - Returns The new matrix
 Matrix tanHDeriv(Matrix matrix) {
   return matrix.performFunction((x) => 1 - pow(x, 2));
 }
 
+///relu of a matrix
+/// - [matrix] The matrix to find the relu
+/// - Returns The new matrix
 Matrix relu(Matrix matrix) {
   return matrix.performFunction((x) => max(0.0, x));
 }
 
+///derivative of a relu matrix
+/// - [matrix] The matrix to find the derivative
+/// - Returns The new matrix
 Matrix reluDeriv(Matrix matrix) {
   return matrix.performFunction((x) => x > 0 ? 1.0 : 0.0);
 }
 
+///leaky relu of a matrix
+/// - [matrix] The matrix to find the leaky relu
+/// - Returns The new matrix
 Matrix leakyRelu(Matrix matrix) {
   return matrix.performFunction((x) => x > 0 ? x : 0.01 * x);
 }
 
+///derivative of a leaky relu matrix
+/// - [matrix] The matrix to find the derivative
+/// - Returns The new matrix
 Matrix leakyDeriv(Matrix matrix) {
   return matrix.performFunction((x) => x > 0 ? 1.0 : 0.01);
 }
 
+///linear of a matrix
+/// - [matrix] The matrix to find the linear
+/// - Returns The new matrix
 Matrix linear(Matrix matrix) {
   return matrix;
 }
 
+///derivative of a linear matrix
+/// - [matrix] The matrix to find the derivative
+/// - Returns The new matrix
 Matrix linearDeriv(Matrix matrix) {
   return fill(1, matrix.getRow(), matrix.getCol());
 }
 
+///finds the derivative function
+/// - [activation] The activation function
+/// - Returns The derivative function
 Matrix Function(Matrix) derivative(Matrix Function(Matrix) activation) {
   final activationMap = {
     sigmoid: sigmoidDeriv,
@@ -340,12 +496,19 @@ Matrix Function(Matrix) derivative(Matrix Function(Matrix) activation) {
   }
 }
 
+///one hot encoding of a matrix
+/// - [value] The value to encode
+/// - [size] The size of the matrix
+/// - Returns The new matrix
 Matrix oneHot(int value, int size) {
   Matrix matrix = zeros(1, size);
   matrix.setAt(0, value, value: 1);
   return matrix;
 }
 
+///converts a list to a matrix
+/// - [values] The list of values
+/// - Returns The new matrix
 Matrix toMatrix(List<dynamic> values) {
   Matrix matrix = Matrix(1, values.length);
   for (var i = 0; i < values.length - 1; i++) {
